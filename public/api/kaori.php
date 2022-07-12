684a79332519dbae26852242cce747eefd594074
zxce3 <memetpea25@gmail.com>
2022-07-13 00:43:10 +0700

    update file
    Signed-off-by: zxce3 <memetpea25@gmail.com>

--- public/api/kaori.php
+++ public/api/kaori.php
@@ -1,1 +1,1 @@
-<?php header('Content-Type: image/jpeg');$json=json_decode(file_get_contents('../../data/kaori.json'),true);$jumlah=count($json);$pilih=$json[rand(0,$jumlah-1)];echo file_get_contents($pilih);
+<?php header("Content-Type: image/jpeg");$json=json_decode(file_get_contents("../../data/kaori.json"),true);$jumlah=count($json);$pilih=$json[rand(0,$jumlah-1)];echo file_get_contents($pilih);
