a78b742f8cf35e1aeae06fab0a85748b9071aaa4
zxce3 <memetpea25@gmail.com>
2022-07-13 02:09:12 +0700

    fix
    add `$currentPageUrl` to index API
    
    Signed-off-by: zxce3 <memetpea25@gmail.com>

--- public/api/index.php
+++ public/api/index.php
@@ -5,6 +5,7 @@ const TARGET_SCAN = "../../data";
 $list = scandir(TARGET_SCAN);
 unset($list[0], $list[1]);
 $list = array_values($list);
+$currentPageUrl = 'http://' . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
 
 header("Content-Type: application/json");
 foreach ($list as &$v) {
@@ -12,6 +13,6 @@ foreach ($list as &$v) {
         if ($v > 1) {
                 unset($v[count($v) - 1]);
         }
-        $v = "http://localhost:8080/api/".implode(".", $v).".php";
+        $v = $currentPageUrl.implode(".", $v).".php";
 }
 echo json_encode($list); 
