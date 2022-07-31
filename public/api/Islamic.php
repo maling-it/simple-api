5d170d933545cad1d629782bca35d27f68ca7aa0
zxce3 <memetpea25@gmail.com>
2022-08-01 03:00:04 +0700

    deleted duplicate file
    - public/api/CyberSpace.php
    - public/api/GameWallp.php
    - public/api/Islamic.php
    - public/api/Mountain.php
    - public/api/Programming.php
    - public/api/Technology.php
    
    Signed-off-by: zxce3 <memetpea25@gmail.com>

--- public/api/Islamic.php
+++
@@ -1,1 +0,0 @@
-<?php header('Content-Type: image/jpeg');$json=json_decode(file_get_contents('../../data/Islamic.json'),true);$jumlah=count($json);$pilih=$json[rand(0,$jumlah-1)];echo file_get_contents($pilih);
--- public/api/Programming.php
+++
@@ -1,1 +0,0 @@
-<?php header('Content-Type: image/jpeg');$json=json_decode(file_get_contents('../../data/Programming.json'),true);$jumlah=count($json);$pilih=$json[rand(0,$jumlah-1)];echo file_get_contents($pilih);
